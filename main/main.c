e309d347606e53382796d20ce3bbd3ed8fe139bb
SimiVoid <szymon.trembecki@onmail.com>
2022-07-11 20:35:59 +0200

    Add basic rtos structure

--- main/main.c
+++ main/main.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void app_main(void)
-{
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "main.h"
+
+void control_step_motor_task(void* pvParameters);
+void control_buzzer_task(void* pvParameters);
+void battery_task(void* pvParameters);
+void bluetooth_control_task(void* pvParameters);
+
+void app_main(void) {
+    xTaskCreate(control_step_motor_task, "control_step_motor_task", 1024, NULL, 2, NULL);
+    xTaskCreate(control_buzzer_task, "control_buzzer_task", 1024, NULL, 0, NULL);
+    xTaskCreate(battery_task, "battery_task", 1024, NULL, 1, NULL);
+    xTaskCreate(bluetooth_control_task, "bluetooth_control_task", 1024, NULL, 1, NULL);
+}
+
+void control_step_motor_task(void* pvParameters) {
+    // Setup pins
+
+    while (1) {
+
+    }
+}
+
+void control_buzzer_task(void* pvParameters) {
+    // Setup pins
+
+    while (1) {
+
+    }
+}
+
+void battery_task(void* pvParameters) {
+    // Setup pins
+
+    while (1) {
+
+    }
+}
+
+void bluetooth_control_task(void* pvParameters) {
 
 }
---
+++ main/main.h
@@ -0,0 +1,13 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#define BUZZER_PIN
+
+#define STEP_MOTOR_DIR_PIN
+#define STEP_MOTOR_STEP_PIN
+#define STEP_MOTOR_ENABLE_PIN
+
+#define BATTERY_VOLTAGE_PIN
+#define VOLTAGE_DIVIDER
+
+#endif
